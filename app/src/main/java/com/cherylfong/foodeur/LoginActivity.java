bb84c809aee88bf92a4a68cc41708bbe1669d6ab
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-01-02 21:34:33 -0800

    remove title bar from login activity

--- app/src/main/java/com/cherylfong/foodeur/LoginActivity.java
+++ app/src/main/java/com/cherylfong/foodeur/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.cherylfong.foodeur;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_login);
+
     }
 }
