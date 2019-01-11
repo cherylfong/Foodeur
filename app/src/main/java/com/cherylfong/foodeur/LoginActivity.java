005c7f3f3da79f684cc7f86ba1c161ce018caf56
cherylfong <42821602+cherylfong@users.noreply.github.com>
2019-01-10 19:38:43 -0800

    remove extra white lines

--- app/src/main/java/com/cherylfong/foodeur/LoginActivity.java
+++ app/src/main/java/com/cherylfong/foodeur/LoginActivity.java
@@ -1,19 +1,21 @@
 package com.cherylfong.foodeur;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_login);
 
     }
